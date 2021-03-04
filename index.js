7209b0b6320edb6f090de556fd7fce97f1fc4ecb
Jon Ekse <jrekse@gmail.com>
2021-03-04 12:55:49 -0800

    added the inquirer and the template for the card

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-console.log("hello world!")
+const inquirer = require('inquirer');
+const jest = require('jest');
+const path = require('path');
+const fs = require('fs');
+const html = require('./lib/constructs')
+
+const promptUser = 
+    [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'list',
+            name: 'job',
+            message: 'What is your role in the team?',
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+        {
+            type: 'input',
+            name: 'ID',
+            message: 'What is your ID number?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your Github username?'
+        },
+        {
+            type: 'list',
+            name: 'again',
+            message: 'Would you like to add another team member?',
+            choices: ['Yes', 'No'],
+        }
+    ];
+function writeToFile(fileName, data){
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+};
+
+function init() {
+    inquirer.prompt(promptUser).then((...inquirerResponses)=>{
+        writeToFile('index.html', gen({...inquirerResponses}))
+    })
+}
+
+init();
---
+++ src/pageTemplate.js
@@ -0,0 +1,12 @@
+const card = 
+`<div class="col-4" id="cardcols">
+     <div class="card" style="width: 18rem;">
+         <div class="card-body">
+             <h5 class="card-title">Name</h5>
+             <h4 class="card-subtitle mb-2">Job</h4>
+             <div class="card-body">ID:</div>
+             <div class="card-body">Email:</div>
+             <div class="card-body">Github:</div>
+         </div>
+     </div>
+ </div>`
